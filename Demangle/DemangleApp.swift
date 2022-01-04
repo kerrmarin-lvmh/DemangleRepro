f4d233169c1bcef533282c5d30955d149c584c99
Kerr Marin Miller <kerr@kerrmarin.com>
2022-01-04 11:16:27 +0000

    Initial Commit

---
+++ Demangle/DemangleApp.swift
@@ -0,0 +1,17 @@
+//
+//  DemangleApp.swift
+//  Demangle
+//
+//  Created by Kerr Marin Miller on 04/01/2022.
+//
+
+import SwiftUI
+
+@main
+struct DemangleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
